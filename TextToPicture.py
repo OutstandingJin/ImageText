17a037c91b454af1738e72a522447a6cd717f0ff
Autstanding <outstandingjin@163.com>
2015-02-10 14:59:10 +0800

    can realize basic functions but ain't good enough require more design

--- TextToPicture.py
+++ TextToPicture.py
@@ -1,40 +1,45 @@
 #-*- coding:UTF-8-*-
-import Image
-import ImageDraw
-import ImageFont
+from PIL import ImageFont, ImageDraw
 import sys
+import Image
 import chardet
+import re
 
-path = '/home/autstanding/Code/Python/resources/'
+path = '/home/autstanding/Code/Python/ImageText/resources/'
 content_text = path + '双城记.txt'
 
 def DrawText():
-    im_base = Image.new("1", (1120, 792), "white")
-    font_global = ImageFont.truetype(path + "truetypefont.ttf", 500)
-    font_local = ImageFont.truetype(path + "truetypefont.ttf", 5)
+    font_size = 5 
+
+    font = ImageFont.truetype(path + "truetypefont.ttf", int(500/font_size))
+    #font = ImageFont.truetype("aria.ttf", 100)
+    im_base = Image.new("1", (int(1120/font_size), int(792/font_size)), "white")
     draw = ImageDraw.Draw(im_base)
-    draw.text((0, 0), unicode('雪', 'UTF-8'), font = font_global)
-    draw.text((500, 0), unicode('国', 'UTF-8'), font = font_global)
-    draw = ImageDraw.Draw(im_top)
+    draw.text((0,0), unicode('雪', 'UTF-8'), font = font)
+    draw.text((int(500/font_size),0), unicode('国', 'UTF-8'), font = font)
     im_base_data = list(im_base.getdata())
+    
+    #font = ImageFont.truetype(path + "truetypefont.ttf", font_size)
+    font = ImageFont.truetype('/media/autstanding/066CA48F6CA47B57/Windows/Fonts/simsun.ttc', font_size)
+    im = Image.new("1", (1120, 792), "white")
     line_space = 1
     line_number = 0
     row_number = 0
     content_traverse_index = 0
-    content = ReadContent()
+    content = ReadContent(path + "双城记.txt")
+    draw = ImageDraw.Draw(im)
 
-# there is cordinate transformation
-
-    for i in range(0, int(len(im_base_data)/25)):
-        if im_base_data[i -] != 255:
-            draw.text((int(i / 1120) * 6, int(i / 5) * 5), content[content_traverse_index++], font = font_local)
+    for i in range(0, len(im_base_data)):
+        if im_base_data[i] != 255:
+            draw.text((int(i % int(1120/font_size)) * font_size,  int(i / int(792/font_size)) * font_size), content[i], font = font)
     del draw
     im.save(path + "test.jpg", "JPEG")
-
+   
 def ReadContent(content_text):
     content = ''
     with open(content_text, 'r') as content_file:
-        content = content_file.read().decode(chardet.detect(content)['encoding'])
+        content = content_file.read()
+        content = content.decode(chardet.detect(content)['encoding'])
     content = re.sub(ur"([^\u4e00-\u9fa5])+",''  , content)
     return content
 
